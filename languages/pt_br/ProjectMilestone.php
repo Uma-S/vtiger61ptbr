dbf46994e3830667974869271b2c68729ed7039f
vctrin <valmir@ttcasolucoes.com.br>
2014-06-22 21:56:20 -0300

    rev14154

---
+++ languages/pt_br/ProjectMilestone.php
@@ -0,0 +1,18 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger. All Rights Reserved.
+ * Contributor(s): Valmir C. Trindade - Translate to Brazilian Portuguese - 19/02/2013 - www.ttcasolucoes.com.br
+ ************************************************************************************/
+$languageStrings = array(
+	'LBL_ADD_RECORD' => 'Adicionar Milestone Projeto' ,
+	'LBL_PROJECT_MILESTONE_INFORMATION' => 'Detalhes Milestone Projeto',
+	'LBL_RECORDS_LIST' => 'Lista Milestones Projeto' ,
+	'Milestone Date' => 'Data Milestone', 
+	'Project Milestone Name' => 'Nome Milestone', 
+	'Project Milestone No' => 'No. Milestone' , 
+	'SINGLE_ProjectMilestone'=> 'Milestone Projeto' , 
+);
---
+++ languages/pt_br/Settings/ModuleManager.php
@@ -0,0 +1,47 @@
+<?php
+/*+***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger. All Rights Reserved.
+ * Contributor(s): Valmir C. Trindade - Translate to Brazilian Portuguese - 26/04/2014 - www.ttcasolucoes.com.br
+ *************************************************************************************/
+$languageStrings = array(
+	'LBL_ACCEPT_AND_INSTALL' => 'Aceitar e Instalar',
+	'LBL_ALREADY_EXISTS' => 'Já Existe',
+	'LBL_DECLINE' => 'Rejeitar',
+	'LBL_EXISTS'=>'Existe',
+	'LBL_EXTENSION_NOT_COMPATABLE' => 'A Extensão não é compatível com o Vtiger',
+	'LBL_FAILED' => 'Falhou',
+	'LBL_FINISH' => 'Final',
+	'LBL_IMPORTED_MODULE' => ' o módulo foi importado com sucesso.',
+	'LBL__IMPORTING_MODULE' => 'Importando Módulo',
+	'LBL_IMPORT_MODULE' => 'Instalar a partir Vtiger ...',
+	'LBL_IMPORT_ZIP' => 'Instalar a partir Arquivo ...',
+	'LBL_INSTALLATION' => 'Instalação',
+	'LBL_INSTALLATION_LOG' => 'Log da Instalação',
+	'LBL_INSTALL' => 'Instalar',
+	'LBL_INVALID_FILE' => 'Arquivo Inválido',
+	'LBL_INVALID_IMPORT_TRY_AGAIN' => ' fornecida para importar o módulo! Tente Novamente.',
+	'LBL_LICENSE_ACCEPT_AGREEMENT' => 'Aceito os termos da Licença.',
+	'LBL_LICENSE' => 'Licença',
+	'LBL_MODULE_NAME' => 'Nome Módulo',
+	'LBL_NO_LICENSE_PROVIDED' => 'Nenhuma Licença Fornecida',
+	'LBL_NOT_FOUND' => "Desculpe. Não encontramos nenhuma Extensão/Módulo para Versão Atual",
+	'LBL_OK' => 'OK',
+	'LBL_PROCEED_WITH_IMPORT' => 'Deseja prosseguir com a importação?',
+	'LBL_PUBLISHED_ON' => 'Publicado em',
+	'LBL_PUBLISHER' => 'Editor',
+	'LBL_REQ_VTIGER_VERSION' => 'Versão Vtiger',
+	'LBL_SUCCESSFULL' => 'Successo',
+	'LBL_UPDATED_MODULE' => ' o Módulo fou atualizado com sucesso.',
+	'LBL_UPDATE_MODULE_FROM_FILE' => 'Atualizar Módulo a partir do Arquivo',
+	'LBL__UPDATING_MODULE' => 'Atualizando Módulo',
+	'LBL_UPGRADE' => 'Atualizar',
+	'LBL_VERIFY_IMPORT_DETAILS' => 'Verificar Detalhes Importação',
+	'LBL_VERSION_NOT_SUPPORTED' => 'A versão do Módulo não é compatível com a versão do vtiger',
+	'LBL_VERSION' => 'Versão',
+	'LBL_VTIGER_EXTENSION_STORE' => 'Loja de Extensões do Vtiger',
+
+);
