9dc069062ebc8e0feafb3e85d00fd29b21612638
vctrin <valmir@ttcasolucoes.com.br>
2014-07-05 12:02:20 -0300

    Ajustes conforme Revisão 14162

--- languages/pt_br/Settings/ModuleManager.php
+++ languages/pt_br/Settings/ModuleManager.php
@@ -5,18 +5,19 @@
  * The Original Code is:  vtiger CRM Open Source
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger. All Rights Reserved.
- * Contributor(s): Valmir C. Trindade - Translate to Brazilian Portuguese - 26/04/2014 - www.ttcasolucoes.com.br
+ * Contributor(s): Valmir C. Trindade - Translate to Brazilian Portuguese - 04/07/2014 - www.ttcasolucoes.com.br
  *************************************************************************************/
 $languageStrings = array(
 	'LBL_ACCEPT_AND_INSTALL' => 'Aceitar e Instalar',
-	'LBL_ALREADY_EXISTS' => 'Já Existe',
 	'LBL_DECLINE' => 'Rejeitar',
 	'LBL_EXISTS'=>'Existe',
 	'LBL_EXTENSION_NOT_COMPATABLE' => 'A Extensão não é compatível com o Vtiger',
+        'LBL_EXTENSION_STORE' => 'Loja de Extensões',
 	'LBL_FAILED' => 'Falhou',
 	'LBL_FINISH' => 'Final',
 	'LBL_IMPORTED_MODULE' => ' o módulo foi importado com sucesso.',
 	'LBL__IMPORTING_MODULE' => 'Importando Módulo',
+	'LBL_IMPORT_MODULE' => 'Importar Módulo',
 	'LBL_IMPORT_MODULE' => 'Instalar a partir Vtiger ...',
 	'LBL_IMPORT_ZIP' => 'Instalar a partir Arquivo ...',
 	'LBL_INSTALLATION' => 'Instalação',
@@ -30,6 +31,7 @@ $languageStrings = array(
 	'LBL_NO_LICENSE_PROVIDED' => 'Nenhuma Licença Fornecida',
 	'LBL_NOT_FOUND' => "Desculpe. Não encontramos nenhuma Extensão/Módulo para Versão Atual",
 	'LBL_OK' => 'OK',
+        'LBL_PHP_EXTENSION_LOADER_IS_NOT_AVAIABLE' => 'Por favor, instale o carregador de extensão',
 	'LBL_PROCEED_WITH_IMPORT' => 'Deseja prosseguir com a importação?',
 	'LBL_PUBLISHED_ON' => 'Publicado em',
 	'LBL_PUBLISHER' => 'Editor',
@@ -44,4 +46,12 @@ $languageStrings = array(
 	'LBL_VERSION' => 'Versão',
 	'LBL_VTIGER_EXTENSION_STORE' => 'Loja de Extensões do Vtiger',
 
+);
+$jsLanguageStrings = array(
+  'JS_ON' => 'em',
+  'JS_PLEASE_INSTALL_EXTENSION_LOADER_TO_INSTALL_THIS_EXTENSION_FROM_BELOW_LINK' => 'Por favor, para instalar esta extensão instale o carregador de extensão a partir do link abaixo',
+  'JS_EXTENSION_STORE_IS_NOT_SUPPOTED_WOULD_YOU_LIKE_TO_INSTALL' => 'A extensão não é suportada, gostaria de instalar?',
+  'JS_PHP_EXTENSION_LOADER_NOT_INSTALLED' => 'O carregador de extensão PHP não foi instalado',
+  'JS_CLICK_HERE' => 'Clique aqui ',
+  'JS_FOR_INSTALLATION' => 'para instalar',
 );
