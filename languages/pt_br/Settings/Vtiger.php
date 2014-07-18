df8383a1e56fe4f2244cfd5733917970652c4d83
vctrin <valmir@ttcasolucoes.com.br>
2014-07-18 01:42:55 -0300

    Adjustments

--- languages/pt_br/Settings/Vtiger.php
+++ languages/pt_br/Settings/Vtiger.php
@@ -5,7 +5,7 @@
  * The Original Code is:vtiger CRM Open Source
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger. All Rights Reserved.
- * Contributor(s): Valmir C. Trindade - Translate to Brazilian Portuguese - 20/06/2014 - www.ttcasolucoes.com.br
+ * Contributor(s): Valmir C. Trindade - Translate to Brazilian Portuguese - 17/07/2014 - www.ttcasolucoes.com.br
  ************************************************************************************/
 $languageStrings = array(
 	'address' => 'Endereço',
